bfed4ae62f18cc8a51c018c24d974da3000d2027
Wesley Ng <wesjng+learnco@gmail.com>
2018-11-17 05:21:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  var newArray = kittens.slice(1, kittens.length-);
+  var newArray = kittens.slice(1, kittens.length-1);
   return newArray;
 }
 
